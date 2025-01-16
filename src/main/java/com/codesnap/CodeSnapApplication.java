adff6357840a46c8df61dd402591bb650d12b5fb
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-16 04:30:30 +0000

    Adiciona classe principal CodeSnapApplication com anotação @SpringBootApplication

---
+++ src/main/java/com/codesnap/CodeSnapApplication.java
@@ -0,0 +1,12 @@
+package com.codesnap; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class CodeSnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSnapApplication.class, args);
+    }
+}
