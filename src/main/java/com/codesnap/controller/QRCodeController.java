86182088148a0185f1cc087b75a41352468ee620
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-16 04:30:43 +0000

    Atualiza QRCodeController para incluir validações e ajustes nos endpoints

--- src/main/java/com/codesnap/controller/QRCodeController.java
+++ src/main/java/com/codesnap/controller/QRCodeController.java
@@ -1,20 +1,70 @@
 package com.codesnap.controller;
 
 import com.codesnap.service.QRCodeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/qrcode")
+@Validated
 public class QRCodeController {
 
-    @Autowired
-    private QRCodeService qrCodeService;
+    private final QRCodeService qrCodeService;
+
+    public QRCodeController(QRCodeService qrCodeService) {
+        this.qrCodeService = qrCodeService;
+    }
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateQRCode(@RequestBody String data) {
-        String qrCodeBase64 = qrCodeService.generateQRCode(data);
-        return ResponseEntity.ok(qrCodeBase64);
+    public ResponseEntity<byte[]> generateQRCode(@RequestBody @Valid QRCodeRequest request) {
+        byte[] qrCode = qrCodeService.generateQRCode(request.getData(), request.getWidth(), request.getHeight());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "image/png");
+
+        return new ResponseEntity<>(qrCode, headers, HttpStatus.OK);
+    }
+
+    public static class QRCodeRequest {
+        @NotBlank(message = "O campo 'data' não pode estar vazio.")
+        private String data;
+
+        @Min(value = 1, message = "A largura deve ser maior que 0.")
+        private int width;
+
+        @Min(value = 1, message = "A altura deve ser maior que 0.")
+        private int height;
+
+        // Getters e setters
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
     }
 }
